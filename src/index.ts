c626ba43cf8557a14eb80d7c6aa95cd997b9f1c3
Luann <luannmcurioso@gmail.com>
2023-03-11 19:20:06 -0400

    feat: changed to typescript and jit compiling

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express, { Request, Response, NextFunction } from "express";
+
+const app = express();
+
+app.get("/status", (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).send({ foo: "bar" });
+});
+
+
+app.listen(3000, () => {
+  console.log("Aplicação no ar, porta 3000!")
+});
+
